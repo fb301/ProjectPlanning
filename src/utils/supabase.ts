d2ac516e500553aea8bd3309c0ae7a129ab6b9a4
GnomezHub <danny.gomez.mail@gmail.com>
2025-08-18 10:09:40 +0200

    non anon key

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase environment variables");

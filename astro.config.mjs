dbaf0a515eb961c9d43b1524500759d8eb9fad44
fb301 <fb301@proton.me>
2025-08-21 11:34:16 +0200

    bugfix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
-import vue from '@astrojs/vue';
-import netlify from '@astrojs/netlify';
-import remarkMermaid from "remark-mermaidjs";
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@astrojs/vue";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), vue()],
-  
-  markdown: {
-    remarkPlugins: [remarkMermaid],
-  },
-  
+
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  adapter: netlify()
+  adapter: netlify(),
 });
